eb219502bcdcbaf401aced1ac02d9dc5e4d96075
Vandan Agarwal <vandanrkt@gmail.com>
2018-07-11 11:10:11 +0530

    fixing the top navigation

--- NEW/js/menu.js
+++ NEW/js/menu.js
@@ -1,9 +1,9 @@
 //in
 function inCat(){
-    document.getElementById("myNav").style.height = 100%;
+    document.getElementById("myNav").style.height = "100%";
 }
 
 //out
 function outCat(){
-    document.getElementById("myNav").style.height = 0%;
+    document.getElementById("myNav").style.height = "0%";
 }
